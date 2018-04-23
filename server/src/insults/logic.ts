9ecfa4074b35bea032ec248d180df6093df188b7
Charise Walraven <cherri.walraven@gmail.com>
2018-04-23 17:18:43 +0200

    Test

---
+++ server/src/insults/logic.ts
@@ -0,0 +1,14 @@
+let chances = 3
+
+export const calculateChances = (correctComeback, userComeback) => {
+    console.log("correct comeback :" + correctComeback)
+    console.log("user entered comeback :" + userComeback)
+    if (!(correctComeback === userComeback)) {
+        chances = chances - 1;
+    }
+    else {
+        console.log("keep it up!")
+    }
+    return chances
+}
+
